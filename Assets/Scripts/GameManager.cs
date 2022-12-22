06bd85c3179574241c54f122478cd482bccc8862
LaszloPPP <laszlo.k.papp@gmail.com>
2022-12-22 15:51:21 +0100

    Save system done. Floating text started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     private void Awake()
     {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return; //code creates a game manager object in every scene we enter. if we return to the Main scene where there is already a game manager it will
+            //be duplicated. this IF check if it!s already there and destroyz one
+        }
+
         instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
     }
     //resources
     public List<Sprite> playerSprites;
@@ -25,14 +35,36 @@ public class GameManager : MonoBehaviour
     public int experience;
 
     //save state
+    /* what do we need to save?
+     * INT preferredSkin
+     * INT money
+     * INT experience
+     * INT weaponLevel
+     */
+
     public void SaveState()
-    { 
-    
+    {
+        string s = "";
+        s += "0" + "|"; //will be the preferredSkin
+        s += money.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0"; //will be the weaponlevel
+
+        PlayerPrefs.SetString("SaveSate",s);
     }
 
-    public void LoadState()
-    { 
-    
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+        {
+            return;
+        }
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+        //change player skin
+        money = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //change weaponlevel
     }
 
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -9,6 +9,7 @@ public class Portal : Collidable
         if (coll.name == "Player")
         {
             //teleport the player
+            GameManager.instance.SaveState();
             string goToScene = sceneNames[Random.Range(0, sceneNames.Length)];
             UnityEngine.SceneManagement.SceneManager.LoadScene(goToScene);
         }
